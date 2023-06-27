76838e6e15e071156140e1142577cd97a6ef82d6
Bisaso Ronald <bronnie313@gmail.com>
2023-06-27 12:54:27 +0300

    add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './styles/Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+        <button className="header-btn back" type="button">{'<'}2015</button>
+        <ul>
+            <li><button className="header-btn" type="button"><img className="header-icon" src="./mic.jpg" alt="MIC" /></button></li>
+            <li><button className="header-btn" type="button"><img className="header-icon2" src="./setting.jpg" alt="SETTING" /></button></li>
+        </ul>
+    </nav>
   )
 }
 
